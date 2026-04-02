b14774c8e091586848771c32fb738ed92b1d182e
Merin-Pappachan <merinchirayath10@gmail.com>
2026-04-02 11:36:31 +0530

    added CORS to frontend domain

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://my-blog-sandy-beta.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
